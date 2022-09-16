76ab6ba975d24afad26b53af42b9ce3cd7ab14b2
chou <metagym960324@gmail.com>
2022-09-16 12:03:52 +0800

    CHOU

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: './login',
+    component: () => import('../views/BootstrapLogin.vue'),
+  },
 ];
 
 const router = createRouter({
